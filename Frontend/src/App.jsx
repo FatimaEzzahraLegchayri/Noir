a029c6e5718a4e123278f776bcd2b84194ff9412
Fatima Ezzahra Legchayri <fatimaezzahra.legchayri@gmail.com>
2025-02-23 19:13:38 +0100

    signup page with passwoord criteria and password strength

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,8 @@
+import { Route, Routes } from "react-router-dom"
+
+
 import FloatingShape from "./Components/FloatingShape"
+import SignUp from "./Pages/SignUp"
 
 function App() {
 
@@ -9,6 +13,14 @@ function App() {
       <FloatingShape color='bg-gray-400' size='w-64 h-64' top='-5%' left='10%' delay={0} />
 			<FloatingShape color='bg-gray-300' size='w-48 h-48' top='70%' left='80%' delay={5} />
 			<FloatingShape color='bg-gray-400' size='w-32 h-32' top='40%' left='-10%' delay={2} />
+    
+    <Routes>
+      <Route path="/" element={'home'}/>
+      <Route path="/signup" element={<SignUp/>} />
+      <Route path="/signin" element={'signin'}/>
+    </Routes>
+    
+    
     </div>
   )
 }
